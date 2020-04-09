5d9c45d8c0e813d6da5e48a9f96707084943a318
Duyen Tran <ly.tran2@mail.dcu.ie>
2020-04-09 17:09:19 +0100

    fixed scene file

--- project/images/query/timeline.py
+++ project/images/query/timeline.py
@@ -50,33 +50,36 @@ def get_full_scene_from_image(image, group_factor='group'):
         return [img for img in group_info[date][group_id][scene_id]]
 
 def get_multiple_scenes_from_images(begin_image, end_image, group_factor='group'):
+    min_group = grouped_info_dict[begin_image]["group"]
+    max_group = grouped_info_dict[end_image]["group"]
+    date = min_group.split("_")[0]
+    min_group = int(min_group.split("_")[1])
+    max_group = int(max_group.split("_")[1])
+    group_range = range(min_group, max_group + 1)
+
     if group_factor == "group":
-        min_group = grouped_info_dict[begin_image]["group"]
-        max_group = grouped_info_dict[end_image]["group"]
-        date = min_group.split("_")[0]
-        min_group = int(min_group.split("_")[1])
-        max_group = int(max_group.split("_")[1])
-        group_range = range(min_group, max_group + 1)
         images = []
         for index in group_range:
             new_group_id = f"{date}_{index}"
+            print(new_group_id)
             if new_group_id in group_info[date]:
                 images.extend([img for scene in group_info[date][new_group_id]
                                 for img in group_info[date][new_group_id][scene]])
     else:
         min_scene = grouped_info_dict[begin_image]["scene"]
         max_scene = grouped_info_dict[end_image]["scene"]
-        date = min_scene.split("_")[0]
         min_scene = int(min_scene.split("_")[1])
         max_scene = int(max_scene.split("_")[1])
-        scene_range = range(min_scene, max_scene + 1)
         images = []
-        for index in scene_range:
-            new_scene_id = f"{date}_{index}"
-            if new_scene_id in scene_info:
-                images.extend(scene_info[new_scene_id]["images"])
+        scene_range = range(min_scene, max_scene + 1)
+        for index in group_range:
+            new_group_id = f"{date}_{index}"
+            print(new_group_id)
+            if new_group_id in group_info[date]:
+                images.extend([img for scene in group_info[date][new_group_id]
+                                for img in group_info[date][new_group_id][scene] if int(scene.split('_')[1]) in scene_range])
     return images
 
 
 # For ARRON, use something like this
-print(get_multiple_scenes_from_images("2016-08-15/20160815_052416_000.jpg", "2016-08-15/20160815_060939_000.jpg", group_factor="group"))
+print(get_multiple_scenes_from_images("2016-08-23/20160823_122201_000.jpg", "2016-08-23/20160823_122201_000.jpg", group_factor="group"))
